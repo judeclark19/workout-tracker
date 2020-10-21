0a140789fa6889798782a05679d8e3ff22a747e2
Jude Clark <judeclark2019@gmail.com>
2020-10-21 08:47:56 -0400

    server setup

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose connected");
+});
+
+connection.on("error", (err) => {
+  console.log("ERROR connecting mongoose");
+});
+
+app.get("/api/config", (req, res) => {
+  res.json({ success: true });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
