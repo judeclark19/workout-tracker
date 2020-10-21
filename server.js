93458132b5b8c21304711b652a5d126b34bae041
Jude Clark <judeclark2019@gmail.com>
2020-10-21 19:53:20 -0400

    Raj has enlightened me about the single model

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const app = express();
-const PORT = 8080;
+
+const PORT = process.env.PORT || 3000;
+
+// const ingredientsController = require("./controllers/ingredientsController");
+// const pizzaController = require("./controllers/pizzaController")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 const connection = mongoose.connection;
@@ -23,8 +29,11 @@ connection.on("error", (err) => {
 });
 
 app.get("/api/config", (req, res) => {
-  res.json({ success: true });
+  res.json({ success: "Workout homework connected" });
 });
+
+// app.use(ingredientsController);
+// app.use(pizzaController);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
