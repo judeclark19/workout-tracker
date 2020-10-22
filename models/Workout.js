0e4e07ce5a9b33f1417418ff24c606f28167f620
Jude Clark <judeclark2019@gmail.com>
2020-10-22 19:27:34 -0400

    need to get up to heroku

--- models/Workout.js
+++ models/Workout.js
@@ -24,6 +24,12 @@ const WorkoutSchema = new Schema(
   }
 );
 
+WorkoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
